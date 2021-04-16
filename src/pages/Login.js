d407003296dd23e62111524d79cf4129d2d0ba95
ayoung <ayoung.jang@paletti.ai>
2021-04-16 18:43:00 +0900

    eject

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class LoginPage extends React{
+    render(){
+        return(
+            <div>
+                Login
+            </div>
+        )
+    }
+}
---
+++ src/router/RootRouter.js
@@ -0,0 +1,30 @@
+import { observer } from 'mobx-react';
+import React, { Component} from 'react'
+import { BrowserRouter } from "react-router-dom"
+import Login from "../pages/Login/Login";
+import HomeRouter from './HomeRouter';
+import cookieStore from '../store/cookieStore';
+import { tokenLogin } from '../controller/loginCtrl';
+
+
+export default @observer class RootRouter extends Component {//token check
+
+  async componentDidMount(){
+    await tokenLogin()
+  }
+
+  render() {
+    const token = cookieStore.token
+    if (token) {
+      return (
+        <BrowserRouter>
+          <HomeRouter />
+        </BrowserRouter>
+      )
+    }
+    return (
+      <Login />
+    )
+  }
+
+}
